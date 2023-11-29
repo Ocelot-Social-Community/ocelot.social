9a909f791c75e4017aeba899281fcdbe186391b8
Hannes Heine <hannes@infinity.labs.ooo>
2023-11-29 10:21:24 +0100

    Update docs/.vuepress/config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,7 +10,7 @@ export default defineUserConfig({
   locales:{
     // The key is the path for the locale to be nested under.
     // As a special case, the default locale can use '/' as its path.
-    '/': {
+    '/en/': {
       lang: 'en-US', // this will be set as the lang attribute on <html>
       title: 'Ocelot.Social',
       // description: ''
