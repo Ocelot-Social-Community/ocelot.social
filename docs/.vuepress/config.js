73b4459a02413c482d2cb5c4f63f46de6e8d76c6
Wolfgang Huß <wolle.huss@pjannto.com>
2023-12-14 14:27:07 +0100

    Change base configuration code to the dreammall.earth example

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,7 +7,7 @@ export default defineUserConfig({
   ...meta,
   theme,
   plugins,
-  base: process.env.VUEPRESS_BASE ?? '/',
+  base: process.env.VUEPRESS_BASE ? `/${process.env.VUEPRESS_BASE}/` : '/',
   locales:{
     // The key is the path for the locale to be nested under.
     // As a special case, the default locale can use '/' as its path.
