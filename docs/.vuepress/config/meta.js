0127925c0018ed690b8a3e107849dcdf0bfee9c0
mahula <lenzmath@posteo.de>
2024-02-17 09:45:43 +0100

    set description and author for seo

--- docs/.vuepress/config/meta.js
+++ docs/.vuepress/config/meta.js
@@ -1,6 +1,7 @@
 export default {
   title: 'Ocelot.Social',
-  // description: 'Free and open-source social network for active citizenship.',
+  description: 'Free and open-source social network for active citizenship.',
+  author: 'busFaktor() e.V.',
   head: [
     ['meta', {name: 'viewport', content: 'width=device-width,initial-scale=1'}]
   ],
