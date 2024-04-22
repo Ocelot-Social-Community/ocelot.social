11399830961d2da67e6fa00b4741c57b1fde0297
Wolfgang Huß <wolle.huss@pjannto.com>
2024-04-22 12:22:16 +0200

    Implement custom fonts

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,4 @@
+import { getDirname, path } from "vuepress/utils"
 import { defineUserConfig } from 'vuepress'
 import { viteBundler } from '@vuepress/bundler-vite'
 
@@ -5,30 +6,20 @@ import meta from './config/meta'
 import theme from './config/theme'
 import plugins from './config/plugins'
 
+const __dirname = getDirname(import.meta.url)
+
 export default defineUserConfig({
   ...meta,
   theme,
   plugins,
   bundler: viteBundler(),
   base: process.env.VUEPRESS_BASE ? `/${process.env.VUEPRESS_BASE}/` : '/',
-  head: [
-    [
-      "link",
-      {
-        // href: "https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@400;500;700&display=swap",
-        href: "public/fonts/Noto_Sans/NotoSans-Regular.ttf",
-        rel: "stylesheet",
-      },
-    ],
-    [
-      "link",
-      {
-        // href: "https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@400;500;700&display=swap",
-        href: "public/fonts/Noto_Sans/NotoSans-Bold.ttf",
-        rel: "stylesheet",
-      },
-    ],
-  ],
+  alias: {
+    '@theme': path.resolve(
+      __dirname,
+      './theme',
+    ),
+  },
   locales: {
     // The key is the path for the locale to be nested under.
     // As a special case, the default locale can use '/' as its path.
