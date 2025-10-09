1f3d7707d02f313337a65033aec1eaa1876e6f41
mahula <lenzmath@posteo.de>
2025-10-09 09:46:41 +0200

    replace deprecated search plugin due to vuepress udate

--- docs/.vuepress/config/theme.js
+++ docs/.vuepress/config/theme.js
@@ -1,12 +1,12 @@
-import { hopeTheme } from 'vuepress-theme-hope'
+import { hopeTheme } from "vuepress-theme-hope";
 
 export default hopeTheme({
-  favicon: 'favicon.ico',
-  logo: '/logo.svg',
-  docsRepo: 'https://github.com/Ocelot-Social-Community/ocelot.social',
-  repo: 'https://github.com/Ocelot-Social-Community/Ocelot-Social',
-  docsBranch: 'master',
-  docsDir: 'docs',
+  favicon: "favicon.ico",
+  logo: "/logo.svg",
+  docsRepo: "https://github.com/Ocelot-Social-Community/ocelot.social",
+  repo: "https://github.com/Ocelot-Social-Community/Ocelot-Social",
+  docsBranch: "master",
+  docsDir: "docs",
   editLink: true,
   lastUpdated: false,
   contributors: false,
@@ -17,160 +17,172 @@ export default hopeTheme({
     center: ["Links"],
     end: ["Language", "Repo", "Outlook", "Search"],
   },
-  locales:{
+  locales: {
     // The key is the path for the locale to be nested under.
     // As a special case, the default locale can use '/' as its path.
-    '/de/': {
-      lang: 'de-DE',
-      title: 'Ocelot.Social',
-      description: 'Ocelot.Social – Freie und Open-Source-Software zum Erstellen und Betreiben deines eigenen sozialen Netzwerks.',
-      copyright: '&copy; von <a href="https://busfaktor.org" target="_blank">busFaktor() e.V.</a> & Freunde | <a href="/de/imprint/">Impressum</a> | <a href="/de/privacy-policy/">Datenschutz</a>',
-      footer: 'Unter der <a class="vt-link link link" href="https://opensource.org/licenses/MIT" target="_blank" rel="noopener noreferrer"><!--[-->MIT Lizenz<!--]--><!----></a> ausgeliefert.',
+    "/de/": {
+      lang: "de-DE",
+      title: "Ocelot.Social",
+      description:
+        "Ocelot.Social – Freie und Open-Source-Software zum Erstellen und Betreiben deines eigenen sozialen Netzwerks.",
+      copyright:
+        '&copy; von <a href="https://busfaktor.org" target="_blank">busFaktor() e.V.</a> & Freunde | <a href="/de/imprint/">Impressum</a> | <a href="/de/privacy-policy/">Datenschutz</a>',
+      footer:
+        'Unter der <a class="vt-link link link" href="https://opensource.org/licenses/MIT" target="_blank" rel="noopener noreferrer"><!--[-->MIT Lizenz<!--]--><!----></a> ausgeliefert.',
       navbar: [
         {
-          text: 'News',
-          link: '/de/news/'
+          text: "News",
+          link: "/de/news/",
         },
         {
-          text: 'Funktionen',
-          link: '/de/features/'
+          text: "Funktionen",
+          link: "/de/features/",
         },
         {
-          text: 'Einstieg',
-          link: '/de/get-started/'
+          text: "Einstieg",
+          link: "/de/get-started/",
         },
         {
-          text: 'Mitmachen',
-          link: '/de/contribute/'
+          text: "Mitmachen",
+          link: "/de/contribute/",
         },
         {
-          text: 'Lernen',
-          link: '/de/learn/' ,
+          text: "Lernen",
+          link: "/de/learn/",
         },
         {
-          text: 'Spenden',
-          link: '/de/donate/'
+          text: "Spenden",
+          link: "/de/donate/",
         },
         {
-          text: 'Kontakt',
-          link: '/de/contact/'
+          text: "Kontakt",
+          link: "/de/contact/",
         },
       ],
     },
-    '/en/': {
-      lang: 'en-US', // this will be set as the lang attribute on <html>
-      title: 'Ocelot.Social',
-      description: 'Ocelot.Social – Free and open source software to create and run your own social network.',
-      copyright: '&copy; by <a href="https://busfaktor.org" target="_blank">busFaktor() e.V.</a> & Friends | <a href="/en/imprint/">Imprint</a> | <a href="/en/privacy-policy/">Privacy Policy</a>',
-      footer: 'Released under the <a class="vt-link link link" href="https://opensource.org/licenses/MIT" target="_blank" rel="noopener noreferrer"><!--[-->MIT License<!--]--><!----></a>.',
+    "/en/": {
+      lang: "en-US", // this will be set as the lang attribute on <html>
+      title: "Ocelot.Social",
+      description:
+        "Ocelot.Social – Free and open source software to create and run your own social network.",
+      copyright:
+        '&copy; by <a href="https://busfaktor.org" target="_blank">busFaktor() e.V.</a> & Friends | <a href="/en/imprint/">Imprint</a> | <a href="/en/privacy-policy/">Privacy Policy</a>',
+      footer:
+        'Released under the <a class="vt-link link link" href="https://opensource.org/licenses/MIT" target="_blank" rel="noopener noreferrer"><!--[-->MIT License<!--]--><!----></a>.',
       navbar: [
         {
-          text: 'News',
-          link: '/en/news/'
+          text: "News",
+          link: "/en/news/",
         },
         {
-          text: 'Features',
-          link: '/en/features/'
+          text: "Features",
+          link: "/en/features/",
         },
         {
-          text: 'Get Started',
-          link: '/en/get-started/'
+          text: "Get Started",
+          link: "/en/get-started/",
         },
         {
-          text: 'Contribute',
-          link: '/en/contribute/'
-        },
-        { 
-          text: 'Learn',
-          link: '/en/learn/' ,
+          text: "Contribute",
+          link: "/en/contribute/",
         },
         {
-          text: 'Donate',
-          link: '/en/donate/'
+          text: "Learn",
+          link: "/en/learn/",
         },
         {
-          text: 'Contact',
-          link: '/en/contact/'
+          text: "Donate",
+          link: "/en/donate/",
+        },
+        {
+          text: "Contact",
+          link: "/en/contact/",
         },
       ],
     },
-    '/es/': {
-      lang: 'es-ES', // se establecerá como el atributo lang en <html>
-      title: 'Ocelot.Social',
-      description: 'Ocelot.Social: software libre y de código abierto para crear y gestionar tu propia red social.',
-      copyright: '&copy; por <a href="https://busfaktor.org" target="_blank">busFaktor() e.V.</a> & Amigos | <a href="/en/imprint/">Aviso legal</a> | <a href="/en/privacy-policy/">Política de Privacidad</a>',
-      footer: 'Liberado bajo la <a class="vt-link link link" href="https://opensource.org/licenses/MIT" target="_blank" rel="noopener noreferrer"><!--[-->Licencia MIT<!--]--><!----></a>.',
+    "/es/": {
+      lang: "es-ES", // se establecerá como el atributo lang en <html>
+      title: "Ocelot.Social",
+      description:
+        "Ocelot.Social: software libre y de código abierto para crear y gestionar tu propia red social.",
+      copyright:
+        '&copy; por <a href="https://busfaktor.org" target="_blank">busFaktor() e.V.</a> & Amigos | <a href="/en/imprint/">Aviso legal</a> | <a href="/en/privacy-policy/">Política de Privacidad</a>',
+      footer:
+        'Liberado bajo la <a class="vt-link link link" href="https://opensource.org/licenses/MIT" target="_blank" rel="noopener noreferrer"><!--[-->Licencia MIT<!--]--><!----></a>.',
       navbar: [
         {
-          text: 'Noticias',
-          link: '/es/news/'
+          text: "Noticias",
+          link: "/es/news/",
         },
         {
-          text: 'Funcionalidades',
-          link: '/es/features/'
+          text: "Funcionalidades",
+          link: "/es/features/",
         },
         {
-          text: 'Empezar',
-          link: '/es/get-started/'
+          text: "Empezar",
+          link: "/es/get-started/",
         },
         {
-          text: 'Contribuir',
-          link: '/es/contribute/'
-        },
-        { 
-          text: 'Aprender',
-          link: '/es/learn/' ,
+          text: "Contribuir",
+          link: "/es/contribute/",
         },
         {
-          text: 'Donar',
-          link: '/es/donate/'
+          text: "Aprender",
+          link: "/es/learn/",
         },
         {
-          text: 'Contacto',
-          link: '/es/contact/'
+          text: "Donar",
+          link: "/es/donate/",
+        },
+        {
+          text: "Contacto",
+          link: "/es/contact/",
         },
       ],
     },
-    
-    '/fr/': {
-      lang: 'fr-FR',
-      title: 'Ocelot.Social',
-      description: 'Ocelot.Social - Logiciel open source et gratuit pour créer et gérer votre propre réseau social.',
-      copyright: '&copy; par <a href="https://busfaktor.org" target="_blank">busFaktor() e.V.</a> & amis | <a href="/fr/imprint/">Mentions légales</a> | <a href="/fr/privacy-policy/">Protection des données</a>',
-      footer: 'Publié sous la <a class="vt-link link link" href="https://opensource.org/licenses/MIT" target="_blank" rel="noopener noreferrer"><!--[-->licence MIT<!--]--><!----></a>.',
+
+    "/fr/": {
+      lang: "fr-FR",
+      title: "Ocelot.Social",
+      description:
+        "Ocelot.Social - Logiciel open source et gratuit pour créer et gérer votre propre réseau social.",
+      copyright:
+        '&copy; par <a href="https://busfaktor.org" target="_blank">busFaktor() e.V.</a> & amis | <a href="/fr/imprint/">Mentions légales</a> | <a href="/fr/privacy-policy/">Protection des données</a>',
+      footer:
+        'Publié sous la <a class="vt-link link link" href="https://opensource.org/licenses/MIT" target="_blank" rel="noopener noreferrer"><!--[-->licence MIT<!--]--><!----></a>.',
       navbar: [
         {
-          text: 'Actualités',
-          link: '/fr/news/'
+          text: "Actualités",
+          link: "/fr/news/",
         },
         {
-          text: 'Fonctionnalités',
-          link: '/fr/features/'
+          text: "Fonctionnalités",
+          link: "/fr/features/",
         },
         {
-          text: 'Commencer',
-          link: '/fr/get-started/'
+          text: "Commencer",
+          link: "/fr/get-started/",
         },
         {
-          text: 'Contribuer',
-          link: '/fr/contribute/'
-        },
-        { 
-          text: 'Apprendre',
-          link: '/fr/learn/' ,
+          text: "Contribuer",
+          link: "/fr/contribute/",
         },
         {
-          text: 'Donner',
-          link: '/fr/donate/'
+          text: "Apprendre",
+          link: "/fr/learn/",
         },
         {
-          text: 'Contact',
-          link: '/fr/contact/'
+          text: "Donner",
+          link: "/fr/donate/",
+        },
+        {
+          text: "Contact",
+          link: "/fr/contact/",
         },
       ],
-    }
+    },
   },
-  
+
   plugins: {
     blog: {
       excerptLength: 0,
@@ -184,34 +196,23 @@ export default hopeTheme({
     },
     redirect: {
       autoLocale: true,
-      defaultBehavior: 'defaultLocale',
-      defaultLocale: '/de/',
+      defaultBehavior: "defaultLocale",
+      defaultLocale: "/de/",
       localeConfig: {
-        '/de/': ['de-DE', 'de'],
-        '/en/': ['en-US', 'en'],
-        '/es/': ['es-ES', 'es'],
-        '/fr/': ['fr-FR', 'fr'],
+        "/de/": ["de-DE", "de"],
+        "/en/": ["en-US", "en"],
+        "/es/": ["es-ES", "es"],
+        "/fr/": ["fr-FR", "fr"],
       },
       localeFallback: false,
     },
-    searchPro: {
+    slimsearch: {
       indexContent: true,
-      autoSuggestions: true,
-      customFields: [
-        {
-          getter: (page) => page.frontmatter.category,
-          formatter: "Category: $content",
-        },
-        {
-          getter: (page) => page.frontmatter.tag,
-          formatter: "Tag: $content",
-        },
-      ],
     },
     seo: {
-      fallbackImage: '/logo.svg',
-      hostname: 'https://ocelot.social/'
-    }
+      fallbackImage: "/logo.svg",
+      hostname: "https://ocelot.social/",
+    },
   },
   iconAssets: "fontawesome",
-})
+});
