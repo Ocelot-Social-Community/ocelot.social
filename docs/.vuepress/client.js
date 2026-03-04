f09031d449dc1d1320af47fc16911af02ab498ce
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-03-04 10:07:33 +0100

    fix redirect

--- docs/.vuepress/client.js
+++ docs/.vuepress/client.js
@@ -1,10 +1,14 @@
 import { defineClientConfig } from "vuepress/client";
 import DonationBar from "./components/DonationBar.vue";
 import MiniBlog from "./components/MiniBlog.vue";
+import RedirectHome from "./components/RedirectHome.vue";
 
 export default defineClientConfig({
   enhance({ app }) {
     app.component("DonationBar", DonationBar);
     app.component("MiniBlog", MiniBlog);
+  },
+  layouts: {
+    RedirectHome,
   },
 });
